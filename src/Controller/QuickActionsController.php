48a34a8a3030ee85eba97bc2fdd73db66d6db804
Stavros Kounis (WSL 22.04) <skounis@gmail.com>
2025-07-14 14:21:28 +0200

    Implement set as homepage

--- src/Controller/QuickActionsController.php
+++ src/Controller/QuickActionsController.php
@@ -4,17 +4,40 @@ namespace Drupal\quick_actions\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Drupal\Core\Url;
+use Drupal\Core\Access\AccessResult;
 use Drupal\node\Entity\Node;
 
+/**
+ * Controller for Quick Actions.
+ */
 class QuickActionsController extends ControllerBase {
 
+  /**
+   * Sets the given node as the front page.
+   *
+   * @param int $node
+   *   The node ID.
+   *
+   * @return \Symfony\Component\HttpFoundation\RedirectResponse
+   */
   public function setHomepage($node) {
     $node = Node::load($node);
+
     if ($node) {
-      // Placeholder logic, not actually setting front page yet.
-      $this->messenger()->addStatus($this->t('Pretending to set %title as homepage.', ['%title' => $node->label()]));
+      // Actually set the front page path.
+      $path = '/node/' . $node->id();
+      \Drupal::configFactory()->getEditable('system.site')
+        ->set('page.front', $path)
+        ->save();
+
+      $this->messenger()->addStatus($this->t('The front page has been set to %title.', ['%title' => $node->label()]));
     }
-    return new RedirectResponse('/node/' . $node->id() . '/edit');
+    else {
+      $this->messenger()->addError($this->t('The requested node could not be found.'));
+    }
+
+    return $this->redirect('entity.node.edit_form', ['node' => $node->id()]);
   }
 
 }
